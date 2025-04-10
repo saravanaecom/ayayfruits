fc07c7768efaac2f3fd422cac8401dc5be2602c6
saravanaecom <saravanaecomweb@gmail.com>
2025-04-10 10:45:53 +0530

    Add files via upload

--- src/components/logo/AppLogo.jsx
+++ src/components/logo/AppLogo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from './logo.png';
 const AppLogo = ({ children }) => {
   return (
-    <img src={Logo} alt="logo" style={{width: '130px', height: '50px'}}/>
+    <img src={Logo} alt="logo" style={{width: '150px', height: '60px'}}/>
   );
 };
 
